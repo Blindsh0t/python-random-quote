2ecdcd5e3fc22fca6c745d5424a6572117f99c4c
Han Patel <han@Hardiks-MacBook-Pro.local>
2020-05-11 12:51:28 +0530

    Added quotes

--- get-quote.py
+++ get-quote.py
@@ -7,7 +7,7 @@ def primary():
     f = open("quotes.txt")
     quotes = f.readlines()
     f.close()
-    last = 13
+    last = len(quotes) - 1
     rnd = random.randint(0, last)
     print(quotes[rnd])
 
